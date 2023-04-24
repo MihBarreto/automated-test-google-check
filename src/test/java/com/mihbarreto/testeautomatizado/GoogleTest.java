2e5d570dd2ba2de0d0a888415b6aa14163b5137e
Michelle Barreto <michelle.pinheirobarreto@gmail.com>
2023-04-24 12:47:18 -0300

    Métodos iniciar() e devePesquisarNoGoogle adicionados

--- src/test/java/com/mihbarreto/testeautomatizado/GoogleTest.java
+++ src/test/java/com/mihbarreto/testeautomatizado/GoogleTest.java
@@ -1,8 +1,15 @@
 package com.mihbarreto.testeautomatizado;
 
+import com.codeborne.selenide.Selenide;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.codeborne.selenide.Selenide.open;
+import static junit.framework.TestCase.assertTrue;
 
 public class GoogleTest {
     private WebDriver driver;
@@ -14,9 +21,20 @@ public class GoogleTest {
         driver= new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(URL_BASE);
+
     }
     @Test
     public void devePesquisarNoGoogle(){
+        iniciar();
+
+        WebElement inputPesquisa = driver.findElement(By.name("q"));
+        inputPesquisa.sendKeys("Batata Frita" + Keys.ENTER);
+
+        String resultado = driver.findElement(By.id("result-stats")).getText();
+        assertTrue(resultado, resultado.contains("Aproximadamente"));
+
+        driver.quit();
+
 
     }
 }
